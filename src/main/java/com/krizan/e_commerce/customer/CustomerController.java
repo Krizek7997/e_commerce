1bc93a3fc155d322e911bd7d5825132a0c288e85
Matej Krizan <krizan.matej79@gmail.com>
2021-12-17 00:27:46 +0100

    Almost finished CustomerController (without update) May end up rewriting the whole thing...

--- src/main/java/com/krizan/e_commerce/customer/CustomerController.java
+++ src/main/java/com/krizan/e_commerce/customer/CustomerController.java
@@ -1,22 +1,65 @@
 package com.krizan.e_commerce.customer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-    @Autowired
-    private CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @PostMapping
     public ResponseEntity addNewCustomer(@RequestBody Customer customer) {
-        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        customerRepository.save(customer);
+        Integer id = customer.getId();
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity updateCustomer(@PathVariable("id") Integer id, @RequestBody Customer customer) {
+        if (customerRepository.existsById(id)) {
+            //  TODO
+            //  Prepisuje mi to id pizdovina
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED)
+                    .body("Customer with id: " + id + " doesn't exist.");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteCustomer(@PathVariable("id") Integer id) {
+        if (customerRepository.existsById(id)) {
+            customerRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED)
+                    .body("Customer with id: " + id + " doesn't exist.");
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity getAllCustomers() {
+        Iterable<Customer> customers = customerRepository.findAll();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getCustomerById(@PathVariable("id") Integer id) {
+        if (customerRepository.existsById(id)) {
+            Optional<Customer> customer = customerRepository.findById(id);
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 }
