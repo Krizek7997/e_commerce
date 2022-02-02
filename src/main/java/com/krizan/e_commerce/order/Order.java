134918babde1c295499945a48a5d4f53e5bd8e3c
Matej Krizan <krizan.matej79@gmail.com>
2022-02-02 21:03:04 +0100

    Created Order.java class I have no idea if it's good

--- src/main/java/com/krizan/e_commerce/order/Order.java
+++ src/main/java/com/krizan/e_commerce/order/Order.java
@@ -1,9 +1,14 @@
 package com.krizan.e_commerce.order;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.krizan.e_commerce.product.Product;
 import lombok.*;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
-
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,4 +23,33 @@ public class Order {
     @GeneratedValue(strategy= GenerationType.AUTO)
     @Nullable
     private Long id;
+
+    @OneToMany
+    @ToString.Exclude
+    private List<Product> products = new ArrayList<>();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Nullable
+    private LocalDate dateCreated;
+
+    @Nullable
+    private Integer numberOfProducts;
+
+    @Nullable
+    private Double totalPrice;
+
+    @NonNull
+    private String status;
+
+    public Double calcTotalPrice() {
+        Double total = 0D;
+        for (Product p : products)
+            total += p.getFinalUnitPrice();
+        return total;
+    }
+
+    public Integer calcNumberOfProducts() {
+        return products.size();
+    }
+
 }
